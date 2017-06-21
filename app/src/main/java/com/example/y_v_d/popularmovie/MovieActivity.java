6e5a5d7023ba03f410f299e06faff7b43c1442f5
caemooshin <mooshin4@gmail.com>
2017-06-21 13:05:57 +0700

    Add Menu Movie

--- app/src/main/java/com/example/y_v_d/popularmovie/MovieActivity.java
+++ app/src/main/java/com/example/y_v_d/popularmovie/MovieActivity.java
@@ -11,6 +11,11 @@ import android.view.MenuItem;
 
 public class MovieActivity extends AppCompatActivity {
 
+    public final static String MOST_POPULAR = "popular";
+    public final static String TOP_RATED = "top_rated";
+
+    private String mSortBy = MOST_POPULAR;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +29,14 @@ public class MovieActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_movie, menu);
+        switch (mSortBy) {
+            case MOST_POPULAR:
+                menu.findItem(R.id.sort_by_most_popular).setChecked(true);
+                break;
+            case TOP_RATED:
+                menu.findItem(R.id.sort_by_top_rated).setChecked(true);
+                break;
+        }
         return true;
     }
 
@@ -32,11 +45,16 @@ public class MovieActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.sort_by_top_rated:
+                mSortBy = TOP_RATED;
+                item.setChecked(true);
+                break;
+            case R.id.sort_by_most_popular:
+                mSortBy = MOST_POPULAR;
+                item.setChecked(true);
+            default:
+                break;
         }
 
         return super.onOptionsItemSelected(item);
