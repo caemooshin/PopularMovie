480e57b1b099be031bfc57c03d79633ae029c276
caemooshin <mooshin4@gmail.com>
2017-06-28 15:26:44 +0700

    Add adapter

--- app/src/main/java/com/example/y_v_d/popularmovie/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/y_v_d/popularmovie/adapters/MovieAdapter.java
@@ -1,36 +1,85 @@
 package com.example.y_v_d.popularmovie.adapters;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieAdapterViewHolder>{
+import com.example.y_v_d.popularmovie.MovieDetailActivity;
+import com.example.y_v_d.popularmovie.R;
+import com.example.y_v_d.popularmovie.models.Movie;
+import com.squareup.picasso.Picasso;
 
-    private final static String LOG_TAG = MovieListAdapter.class.getSimpleName();
+import java.util.ArrayList;
+import java.util.List;
 
-    public MovieAdapter() {
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MyViewHolder> {
+    private List<Movie> movies;
+    private int mLayoutItem;
+    private Context mContext;
+    public static final String ARG_MOVIE = "ARG_MOVIE";
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_item, null);
+        return new MyViewHolder(v);
     }
 
     @Override
-    public MovieAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
-    }
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
 
-    @Override
-    public void onBindViewHolder(MovieAdapter.ViewHolder holder, int position) {
+        holder.mTitle.setText(movies.get(position).getTitle());
 
+        Picasso.with(mContext)
+                .load(movies.get(position).getPosterPath())
+                .config(Bitmap.Config.RGB_565)
+                .into(holder.mThumbnailView);
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, MovieDetailActivity.class);
+                intent.putExtra(ARG_MOVIE, movies.get(position));
+                intent.addFlags(FLAG_ACTIVITY_NEW_TASK);
+                mContext.startActivity(intent);
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return movies.size();
     }
 
-    public class MovieAdapterViewHolder extends RecyclerView.ViewHolder{
+    public class MyViewHolder extends RecyclerView.ViewHolder {
 
-            public MovieAdapterViewHolder(View itemView) {
-                super(itemView);
-            }
+        LinearLayout moviesLayout;
+        TextView mTitle;
+        public final View mView;
+        ImageView mThumbnailView;
 
+
+
+        public MyViewHolder(View v) {
+            super(v);
+
+            mView = v;
+            mThumbnailView = (ImageView) itemView.findViewById(R.id.thumbnail);
+            mTitle = (TextView) itemView.findViewById(R.id.title);
+        }
+    }
+    public MovieAdapter(List<Movie> movies, int mLayoutItem, Context mContext) {
+        this.movies = movies;
+        this.mLayoutItem = mLayoutItem;
+        this.mContext = mContext;
     }
 }
